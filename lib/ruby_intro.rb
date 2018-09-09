412172918d6c9cf8a94ec148ba9fb93b74c52eec
EC2 Default User <ec2-user@ip-172-31-77-163.ec2.internal>
2018-09-09 04:01:13 +0000

    done 1&2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,83 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum=0
+  arr.each{|a| sum+=a}
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr == []
+    return 0
+  elsif arr.length==1
+    return arr[0]
+  else
+    max1=arr.max
+    original_len=arr.length
+    arr.delete_if {|i| i == max1 } 
+    if arr.length <= original_len-2
+      return max1*2
+    else
+      return max1 + arr.max
+    end
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr==[]
+    return false
+  elsif arr.length==1
+    return false
+  end
+  #ref=[]
+  #arr.each{|a| ref+=[n-a]}
+  #if (ref&arr).length==0
+  #  return false
+  #else
+  #  return true
+  #end
+  i=0
+  while i< arr.length
+    j=1
+    while i+j < arr.length
+     if arr[i]+arr[i+j] == n
+       return true
+     end
+     j+=1
+    end
+    i+=1
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  exist = ([s[0]]&%w[A E I O U a e i o u])
+  if exist.length == 0
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0
+    return false
+  end
+  if s.count("0-1") != s.length
+    return false
+  end
+  s_decimal = s.to_i(10)
+  if s_decimal % 4 ==0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
