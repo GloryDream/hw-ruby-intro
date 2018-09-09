e766d7dc0307b7c97488c4440ae09a18cbf2a79e
GXY-AWS <xy_gong@foxmail.com>
2018-09-09 22:19:53 +0000

    all done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -88,7 +88,16 @@ class BookInStock
   def initialize(isbn, price)
     @isbn=isbn
     @price=price
+    if isbn == "" || price <=0
+      raise ArgumentError
+    end
   end
+
   attr_reader :isbn, :price
   attr_writer :isbn, :price
+
+  def price_as_string
+    "$%0.2f" % [@price]
+  end
+
 end
